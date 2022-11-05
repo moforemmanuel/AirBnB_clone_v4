97f122dac28fa4a012c520138af15d811b5bf72a
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-11-05 05:28:28 +0100

    load places from frontend

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -31,4 +31,43 @@ $(function () {
                 : $('div#api_status').removeClass('available');
         }
     });
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://0.0.0.0:5001/api/v1/places_search/',
+        data: '{}',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function(data) {
+            console.log(data);
+            for (let place of data) {
+                // $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+                $('.places').append(
+                    `<article>
+                        <div class="title_box">
+                            <h2>${place.name}</h2>
+                            <div class="price_by_night">$${place.price_by_night}</div>
+                        </div>
+                        <div class="information">
+                            <div class="max_guest">${place.max_guest} Guest${place.max_guest != 1 && 's'}</div>
+                            <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms != 1 && 's'}</div>
+                            <div class="number_bathrooms">${
+                                place
+                                .number_bathrooms
+                            } Bathroom${place.number_bathrooms && 's'}
+                            </div>
+                        </div>
+                        
+                        <div class="description">
+                            ${place.description || 'Description'}
+                        </div>
+                     </article>
+                     `
+                )
+            }
+        }
+
+    })
+
 });
+
